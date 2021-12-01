cee7ead57aea2f3b27e79c0a5c8002d4cfb147af
johnny-as <62854741+johnny-as@users.noreply.github.com>
2021-11-30 21:02:26 -0300

    requisiçoes html

---
+++ Aulas igti/modulo 0/Requisições HTTP/script.js
@@ -0,0 +1,16 @@
+let employeesPromise = fetch('http://localhost:3000/employees');
+//
+
+employeesPromise.then(resp => {
+  resp.json().then(employees => {
+    let table = renderTable(employees);
+    document.getElementById('app').innerHTML = table;
+  });
+});
+
+function renderTable(employees) {
+  employees.map(employee => {
+    return `<tr><td>${employee.id}</td><td>${employee.name}</td></tr>`;
+  });
+  return `<table>${rows.join('')}</table>`;
+}
